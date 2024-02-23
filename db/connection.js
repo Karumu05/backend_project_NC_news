f21855ea326eb2c9eb992ef1ddfd5fec4b67315f
Calum <calum.borders@gmail.com>
2024-02-23 13:01:46 +0000

    Updated files for elephant

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,6 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
-if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
-  throw new Error('PGDATABASE or DATABASE_URL not set');
-}
 
 const config = {}
 
@@ -15,6 +12,10 @@ if (ENV === 'production'){
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');
+}
 
 if (!process.env.PGDATABASE) {
   throw new Error('PGDATABASE not set');
